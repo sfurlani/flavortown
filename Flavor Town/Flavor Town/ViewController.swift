e4ec9513370463dfc73966e72d3695e25cf2ebdc
Stephen Furlani <stephen.furlani@gmail.com>
2020-09-02 14:39:58 -0400

    Adding New Project

---
+++ Flavor Town/Flavor Town/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flavor Town
+//
+//  Created by Stephen Furlani on 9/2/20.
+//  Copyright © 2020 Upside Commerce Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
